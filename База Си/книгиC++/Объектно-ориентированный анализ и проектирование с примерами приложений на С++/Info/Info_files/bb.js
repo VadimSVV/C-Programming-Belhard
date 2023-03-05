250889a3e8f8799959fe13af273a849093ff9fbd
Veydem <51034142+VadimSVV@users.noreply.github.com>
2023-03-05 19:47:15 +0300

    Add files via upload

---
+++ База Си/книгиC++/Объектно-ориентированный анализ и проектирование с примерами приложений на С++/Info/Info_files/bb.js
@@ -0,0 +1,7 @@
+document.writeln ( 
+	"<a target=_blank href='http://ad3.bannerbank.ru/bb.cgi?cmd=go&r=r_ssi" + 
+	"&vbn=453&pac=121808417&pnm=1&bac=6691246&bnm=1826&ref=http%3A%2F%2Fwww%2Emirknig%2Ecom%2Fcategory%2Fprogramming%2Fpage%2F7%2F&loc=http%3A%2F%2Fwww%2Emirknig%2Ecom%2F2006%2F02%2F19%2Fbuch%5Fg%5Fobektnoorientirovannyjj%5Fanaliz%5Fi%5Fproektirovanie%5Fs%5Fprimerami%5Fprilozhenijj%5Fna%5Fs%2Ehtml&htr=http%3A%2F%2Fwww%2Emirknig%2Ecom%2F2006%2F02%2F19%2Fbuch%5Fg%5Fobektnoorientirovannyjj%5Fanaliz%5Fi%5Fproektirovanie%5Fs%5Fprimerami%5Fprilozhenijj%5Fna%5Fs%2Ehtml'" +
+	">" + 
+	"<img src='http://195.161.119.239/images/6691/6691246_1826.gif' border=0 width=120 height=60 alt=\"JBE.ru - 120õ60\" title=\"JBE.ru - 120õ60\"></a>"
+);
+
---
+++ База Си/книгиC++/Объектно-ориентированный анализ и проектирование с примерами приложений на С++/Info/Info_files/bb_002.js
@@ -0,0 +1,7 @@
+document.writeln ( 
+	"<a target=_blank href='http://ad3.bannerbank.ru/bb.cgi?cmd=go&r=r_ssi" + 
+	"&vbn=452&pac=121217723&pnm=1&bac=125395303&bnm=1&ref=http%3A%2F%2Fwww%2Emirknig%2Ecom%2Fcategory%2Fprogramming%2Fpage%2F7%2F&loc=http%3A%2F%2Fwww%2Emirknig%2Ecom%2F2006%2F02%2F19%2Fbuch%5Fg%5Fobektnoorientirovannyjj%5Fanaliz%5Fi%5Fproektirovanie%5Fs%5Fprimerami%5Fprilozhenijj%5Fna%5Fs%2Ehtml&htr=http%3A%2F%2Fwww%2Emirknig%2Ecom%2F2006%2F02%2F19%2Fbuch%5Fg%5Fobektnoorientirovannyjj%5Fanaliz%5Fi%5Fproektirovanie%5Fs%5Fprimerami%5Fprilozhenijj%5Fna%5Fs%2Ehtml'" +
+	">" + 
+	"<img src='http://195.161.119.239/images/125395/125395303_1.gif' border=0 width=468 height=60 alt=\"JBE.RU 468 - реклама в интернет\" title=\"JBE.RU 468 - реклама в интернет\"></a>"
+);
+
